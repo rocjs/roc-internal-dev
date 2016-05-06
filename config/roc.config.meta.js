8631fdcdda82ee49b2bc17f0b714a1766781edcc
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-05-06 16:42:13 +0200

    Added a way to link additional modules when running the link command

--- config/roc.config.meta.js
+++ config/roc.config.meta.js
@@ -1,3 +1,5 @@
+const toArray = require('roc/converters').toArray;
+
 module.exports = {
     commands: {
         'build': { description: 'Builds project' },
@@ -5,7 +7,11 @@ module.exports = {
         'clean': { description: 'Cleans generated files' },
         'docs': { description: 'Generates markdown documentation' },
         'esdocs': { description: 'Generates ESDoc' },
-        'link': { description: 'Links up the project (Not plugin)' },
+        'link': { description: 'Links up the project', arguments: [{
+          name: 'packages',
+          description: 'Modules that should be linked into the packages in packages/',
+          converter: toArray
+        }] },
         'lint:alias': { description: 'Run local lint inside packages (Not plugin)' },
         'lint': { description: 'Runs lint' },
         'release': { description: 'Run release script' }
